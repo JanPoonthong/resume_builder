2ac585c9e8e7a5a9287ad08466a7f83ed04ade54
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-01 10:03:01 +0700

    Add test_script.sh

---
+++ test_script.bash
@@ -0,0 +1,8 @@
+javac -d ./build -cp ./jars/itextpdf-5.5.13.3.jar:jars/pdfbox-app-2.0.27.jar src/*.java
+{
+  echo "Jan Poonthong"
+  echo "janpoonthong@gmail.com"
+  echo "911"
+  echo "profile.jpg"
+  echo "done"
+} | java -cp ./build:./jars/itextpdf-5.5.13.3.jar:jars/pdfbox-app-2.0.27.jar Main
